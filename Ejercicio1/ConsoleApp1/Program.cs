2fccda717dc4115061a2b9bf9eaeea91c4b0332f
Letl1294 <lucasetl94@hotmail.com>
2019-03-21 21:48:33 -0300

    Clase2

---
+++ Ejercicio1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Ingrese un numero: ");
+            num = int.Parse(Console.ReadLine());
+            for (int i = 1; i < num; i++)
+            {
+                if(esCentroNumerico(i))
+                {
+                    Console.WriteLine("Centro numerico: "+i);
+                }
+            }
+            Console.ReadKey();
+        }
+        static bool esCentroNumerico(int numero)
+        {
+            int minimo;
+            bool centroNum;
+            minimo = sumaMinima(numero);
+            centroNum = sumaMaxima(numero,minimo);
+            return centroNum;
+        }
+        static int sumaMinima(int numero)
+        {
+            int acum = 0;
+            for (int i = 1; i < numero; i++)
+                acum = acum + i;
+            return acum;
+        }
+        static bool sumaMaxima(int numero, int minimo)
+        {
+            int acum = 0;
+            int i = numero+1;
+            while (acum < minimo)
+            {
+                acum = acum + i;
+                if (acum == minimo)
+                {
+                    return true;
+                }
+                i++;
+            }
+            return false;
+        }
+    }
+}
--- Ejercicio1/Ejercicio4/Program.cs
+++ Ejercicio1/Ejercicio4/Program.cs
@@ -10,7 +10,26 @@ namespace Ejercicio4
     {
         static void Main(string[] args)
         {
-
+            int acum=0, contador=0;
+            int i=1;
+            while(contador<4)
+            {
+                acum = 0;
+                for (int j=1;j<i;j++)
+                {
+                    if(i%j==0 && i!=j)
+                    {
+                        acum = j + acum;
+                    }
+                }
+                if(acum==i)
+                {
+                    Console.WriteLine("Numero Perfecto: " + acum);
+                    contador++;
+                }
+                i++;
+            }
+            Console.ReadKey();
         }
     }
 }
